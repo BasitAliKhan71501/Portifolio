bb55ccdf9849439ee63b71e0fa458de4b75a7c43
BasitAliKhan71501 <basitalikhanhunzai@gmail.com>
2022-04-14 10:11:02 -0700

    Portifolio

---
+++ js/script.js
@@ -0,0 +1,208 @@
+AOS.init();
+
+
+
+
+let navbar = document.getElementById("nav")
+
+window.onscroll = () => {
+    let scr = window.scrollY;
+    if (scr > 40) {
+        navbar.style.transition = "0.5s";
+        navbar.style.width = "100%";
+        navbar.style.position = "fixed";
+        navbar.style.zIndex = "99999";
+        navbar.style.top = "0";
+        navbar.style.backgroundColor = "black";
+    } else {
+        navbar.style.position = "relative";
+        navbar.style.backgroundColor = "transparent";
+    }
+}
+
+
+$(document).ready(function() {
+    $('.list').click(function() {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.contentBx').show('1000')
+        } else {
+            $('.contentBx').not('.' + value).hide('1000');
+            $('.contentBx').filter('.' + value).show('1000')
+        }
+    })
+
+
+    //Adding Active Class On The Selected Item
+    $('.list').click(function() {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+})
+
+
+
+
+
+
+// set the starting position of the cursor outside of the screen
+let clientX = 100;
+let clientY = 100;
+const innerCursor = document.querySelector(".cursor--small");
+
+const initCursor = () => {
+    // add listener to track the current mouse position
+    document.addEventListener("mousemove", e => {
+        clientX = e.clientX;
+        clientY = e.clientY;
+    });
+
+    // transform the innerCursor to the current mouse position
+    // use requestAnimationFrame() for smooth performance
+    const render = () => {
+        innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+        // if you are already using TweenMax in your project, you might as well
+        // use TweenMax.set() instead
+        // TweenMax.set(innerCursor, {
+        //   x: clientX,
+        //   y: clientY
+        // });
+
+        requestAnimationFrame(render);
+    };
+    requestAnimationFrame(render);
+};
+
+initCursor();
+
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCursor;
+
+const initCanvas = () => {
+    const canvas = document.querySelector(".cursor--canvas");
+    const shapeBounds = {
+        width: 75,
+        height: 75
+    };
+    paper.setup(canvas);
+    const strokeColor = "#dc3545";
+    const strokeWidth = 1;
+    const segments = 8;
+    const radius = 20;
+
+    // we'll need these later for the noisy circle
+    const noiseScale = 150; // speed
+    const noiseRange = 4; // range of distortion
+    let isNoisy = false; // state
+
+    // the base shape for the noisy circle
+    const polygon = new paper.Path.RegularPolygon(
+        new paper.Point(0, 0),
+        segments,
+        radius
+    );
+    polygon.strokeColor = strokeColor;
+    polygon.strokeWidth = strokeWidth;
+    polygon.smooth();
+    group = new paper.Group([polygon]);
+    group.applyMatrix = false;
+
+    const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+    let bigCoordinates = [];
+
+    // function for linear interpolation of values
+    const lerp = (a, b, n) => {
+        return (1 - n) * a + n * b;
+    };
+
+    // function to map a value from one range to another range
+    const map = (value, in_min, in_max, out_min, out_max) => {
+        return (
+            ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+        );
+    };
+
+    // the draw loop of Paper.js 
+    // (60fps with requestAnimationFrame under the hood)
+    paper.view.onFrame = event => {
+        // using linear interpolation, the circle will move 0.2 (20%)
+        // of the distance between its current position and the mouse
+        // coordinates per Frame
+        lastX = lerp(lastX, clientX, 0.2);
+        lastY = lerp(lastY, clientY, 0.2);
+        group.position = new paper.Point(lastX, lastY);
+    }
+}
+
+initCanvas();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+    splitLetters(words[i]);
+}
+
+function changeWord() {
+    var cw = wordArray[currentWord];
+    var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+    for (var i = 0; i < cw.length; i++) {
+        animateLetterOut(cw, i);
+    }
+
+    for (var i = 0; i < nw.length; i++) {
+        nw[i].className = 'letter behind';
+        nw[0].parentElement.style.opacity = 1;
+        animateLetterIn(nw, i);
+    }
+
+    currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+    setTimeout(function() {
+        cw[i].className = 'letter out';
+    }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+    setTimeout(function() {
+        nw[i].className = 'letter in';
+    }, 340 + (i * 80));
+}
+
+function splitLetters(word) {
+    var content = word.innerHTML;
+    word.innerHTML = '';
+    var letters = [];
+    for (var i = 0; i < content.length; i++) {
+        var letter = document.createElement('span');
+        letter.className = 'letter';
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
+    }
+
+    wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
